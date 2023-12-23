89e135c01dd602711766ae217ff96af156600f53
JSCRRS <-->
2023-12-23 20:34:26 +0100

    added controller

---
+++ src/books/books.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateBookDto } from "./dto/create-book.dto";
+import { Book } from "./book.entity";
+import { BooksService } from "./books.service";
+
+@Controller("books")
+export class BooksController {
+    constructor(private readonly booksService: BooksService)
+    {}
+
+    @Post()
+    createBook(@Body() createBookDto: CreateBookDto): Promise<Book> {
+        return this.booksService.createBook(createBookDto);
+    }
+
+    @Get()
+    findAllBooks(): Promise<Book[]> {
+        return this.booksService.findAllBooks(); 
+    }
+}
