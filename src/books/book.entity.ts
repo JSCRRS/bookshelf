a5836b080601093e5a7b202746da63c77a970072
JSCRRS <-->
2023-12-23 20:34:39 +0100

    added entity

---
+++ src/books/book.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Book {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column()
+    author: string
+
+    @Column()
+    title: string;
+
+    @Column()
+    year: number
+}
