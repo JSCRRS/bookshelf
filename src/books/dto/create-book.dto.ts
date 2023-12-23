c3166a5af1870fb858587cbbe1baa353da34fdc2
JSCRRS <-->
2023-12-23 21:12:40 +0100

    added validation pipes

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class CreateBookDto {
+    @IsNotEmpty()
+    @IsString()
     author: string;
+
+    @IsNotEmpty()
+    @IsString()
     title: string;
+
+    @IsNotEmpty()
+    @IsNumber()
     year: number;
 }
