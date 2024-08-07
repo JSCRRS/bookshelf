8970a95a67a0b3eb709c6e0ccc049d753ebe97a9
JSCRRS <-->
2024-08-07 21:25:46 +0200

    added e2e test for POST /authors

--- test/authors.e2e-spec.ts
+++ test/authors.e2e-spec.ts
@@ -1,15 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-//import * as request from 'supertest';
+import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { CreateAuthorDto } from '../src/authors/dto/create-author.dto';
+import { AuthorsService } from '../src/authors/authors.service';
 
 describe('Authors (e2e)', () => {
+  const author: CreateAuthorDto = {
+    firstName: 'A',
+    lastName: 'B',
+  };
+
   let app: INestApplication;
+  let authorsService = {
+    createAuthor: () => {
+      return { id: '1', ...author };
+    },
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(AuthorsService)
+      .useValue(authorsService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
@@ -19,10 +34,11 @@ describe('Authors (e2e)', () => {
     await app.close();
   });
 
-  it('/ (GET)', () => {
-    /* return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!'); */
+  it('creates an author', () => {
+    return request(app.getHttpServer())
+      .post('/authors')
+      .send(author)
+      .expect(201)
+      .expect(authorsService.createAuthor());
   });
 });
