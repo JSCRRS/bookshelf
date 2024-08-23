fd1491eaa5e0949f49236ef352ac4cfd62fe9265
JSCRRS <-->
2024-08-23 22:03:42 +0200

    added e2e test for GET /health endpoints

---
+++ test/health.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import * as request from 'supertest';
+
+describe('Health (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('checks health for http and returns OK', () => {
+    return request(app.getHttpServer()).get('/health').expect(200);
+  });
+
+  it('checks health for db and returns OK', () => {
+    return request(app.getHttpServer()).get('/health/db').expect(200);
+  });
+});
