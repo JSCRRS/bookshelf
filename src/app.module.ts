5579f6f8b8d299ee73520b11fdd7a39466b4b86d
JSCRRS <-->
2024-08-30 22:57:08 +0200

    added configuration variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,18 @@ import { Author } from './authors/author.entity';
 import { GenresModule } from './genres/genres.module';
 import { Genre } from './genres/genre.entity';
 import { HealthModule } from './health/health.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'bookshelf',
-      password: 'root',
-      database: 'books',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Author, Genre],
       synchronize: true,
     }),
