35b839269a582bf1355fdcf82240538f75d2f921
JSCRRS <-->
2024-09-13 21:46:42 +0200

    added entity for relationship

--- src/publishers/publishers.module.ts
+++ src/publishers/publishers.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Publisher } from './publisher.entity';
 import { PublishersService } from './publishers.service';
 import { PublishersController } from './publishers.controller';
+import { Book } from '../books/book.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Publisher])],
+  imports: [TypeOrmModule.forFeature([Publisher, Book])],
   providers: [PublishersService],
   controllers: [PublishersController],
   exports: [PublishersService],
