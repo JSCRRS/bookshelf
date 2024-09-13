ffc3948b92f45b88e9a9d5fd00c9839ccd2eed97
JSCRRS <-->
2024-09-13 21:05:02 +0200

    changed column position in book entity

--- src/books/book.entity.ts
+++ src/books/book.entity.ts
@@ -12,12 +12,12 @@ export class Book {
   @PrimaryGeneratedColumn('uuid')
   public id: string;
 
-  @Column({ type: 'varchar', length: 100 })
-  public title: string;
-
   @ManyToMany(() => Author, (author) => author.books, { eager: true })
   @JoinTable({ name: 'book_author_relations' })
   public authors: Author[];
+
+  @Column({ type: 'varchar', length: 100 })
+  public title: string;
 
   @Column({ type: 'year' })
   public year: string;
