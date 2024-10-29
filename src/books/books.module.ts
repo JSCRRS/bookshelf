8b426015dfcb63d8d1cd4365dee0c7f6d585bd76
JSCRRS <-->
2024-10-29 18:31:20 +0100

    added dependencies to books module

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -4,8 +4,13 @@ import { Book } from './book.entity';
 import { Author } from '../authors/author.entity';
 import { Genre } from '../genres/genre.entity';
 import { Publisher } from '../publishers/publisher.entity';
+import { BooksService } from './books.service';
+import { BooksController } from './books.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book, Author, Publisher, Genre])],
+  providers: [BooksService],
+  controllers: [BooksController],
+  exports: [BooksService],
 })
 export class BooksModule {}
