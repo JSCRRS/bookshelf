8f2cea91605d6da0b2e50afdc64bc6a104ef136f
JSCRRS <-->
2024-10-29 18:31:44 +0100

    added tests for books

---
+++ src/books/books.service.spec.ts
@@ -0,0 +1,84 @@
+import { Repository } from 'typeorm';
+import { BooksService } from './books.service';
+import { Book } from './book.entity';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateBookDto } from './dto/create-book.dto';
+
+const bookId = '111aa111-a11a-111a-a111-11111a111a11';
+const bookTitle = 'test';
+const authorIds = ['111aa111-a11a-111a-a111-11111a111a11'];
+
+const createBookDto: CreateBookDto = {
+  authorIds: authorIds,
+  title: 'test',
+};
+
+const author = {
+  id: '111aa111-a11a-111a-a111-11111a111a11',
+  firstName: 'A',
+  lastName: 'B',
+  birthDate: '2000-01-01',
+  cityOfBirth: 'city',
+  countryOfBirth: 'country',
+};
+
+const book = {
+  id: bookId,
+  title: createBookDto.title,
+  authors: [
+    {
+      id: author.id,
+    },
+  ],
+};
+
+describe('BooksService', () => {
+  let booksService: BooksService;
+  let repository: Repository<Book>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        BooksService,
+        {
+          provide: getRepositoryToken(Book),
+          useValue: {
+            findOneBy: jest.fn().mockResolvedValue(book),
+            findOne: jest.fn().mockResolvedValue(book),
+            save: jest.fn().mockResolvedValue(book),
+          },
+        },
+      ],
+    }).compile();
+    booksService = module.get<BooksService>(BooksService);
+    repository = module.get<Repository<Book>>(getRepositoryToken(Book));
+  });
+
+  describe('createBook()', () => {
+    it('saves and returns a book', () => {
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue(null);
+      expect(booksService.createBook(createBookDto)).resolves.toEqual(book);
+    });
+    it('throws an error, if book already exists', () => {
+      expect(booksService.createBook(createBookDto)).rejects.toThrow(
+        Error(`Book with title '${createBookDto.title}' already exists.`),
+      );
+    });
+  });
+
+  describe('getBookByIdOrTitle', () => {
+    it('gets and returns a book by id', () => {
+      expect(booksService.getBookByIdOrTitle(bookId)).resolves.toEqual(book);
+    });
+    it('gets and returns a book by title', () => {
+      expect(booksService.getBookByIdOrTitle(bookTitle)).resolves.toEqual(book);
+    });
+    it('throws an error, if book does not exist', () => {
+      jest.spyOn(repository, 'findOne').mockResolvedValue(null);
+      expect(booksService.getBookByIdOrTitle(bookId)).rejects.toThrow(
+        Error(`Could not find book with id or title '${bookId}'.`),
+      );
+    });
+  });
+});
---
+++ test/books.e2e-spec.ts
@@ -0,0 +1,80 @@
+import { INestApplication } from '@nestjs/common';
+import { CreateBookDto } from '../src/books/dto/create-book.dto';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { BooksService } from '../src/books/books.service';
+import * as request from 'supertest';
+
+describe('Books (e2e)', () => {
+  const bookId = '111aa111-a11a-111a-a111-11111a111a11';
+  const bookTitle = 'test';
+  const authorIds = ['111aa111-a11a-111a-a111-11111a111a11'];
+
+  const createBookDto: CreateBookDto = {
+    authorIds: authorIds,
+    title: 'test',
+  };
+
+  const author = {
+    id: '111aa111-a11a-111a-a111-11111a111a11',
+    firstName: 'A',
+    lastName: 'B',
+    birthDate: '2000-01-01',
+    cityOfBirth: 'city',
+    countryOfBirth: 'country',
+  };
+
+  const book = {
+    id: bookId,
+    title: createBookDto.title,
+    authors: [
+      {
+        id: author.id,
+      },
+    ],
+  };
+
+  let app: INestApplication;
+
+  const booksService = {
+    createBook: () => book,
+    getBookByIdOrTitle: () => book,
+  };
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(BooksService)
+      .useValue(booksService)
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('creates a book', () => {
+    return request(app.getHttpServer())
+      .post('/books')
+      .send(createBookDto)
+      .expect(201)
+      .expect(booksService.createBook());
+  });
+
+  it('gets a book by id', () => {
+    return request(app.getHttpServer())
+      .get(`/books/${bookId}`)
+      .expect(200)
+      .expect(booksService.getBookByIdOrTitle());
+  });
+  it('gets a book by title', () => {
+    return request(app.getHttpServer())
+      .get(`/books/${bookTitle}`)
+      .expect(200)
+      .expect(booksService.getBookByIdOrTitle());
+  });
+});
